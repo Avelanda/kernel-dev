d2ebd8c458dd5426e43a60cfebf3da5431e8c7ef
Avelanda <gorthell@gmail.com>
2026-07-04 15:11:06 +0200

    Update trace_kprobe_selftest.c
    Scaling optimized data for performance.

--- kernel/trace/trace_kprobe_selftest.c
+++ kernel/trace/trace_kprobe_selftest.c
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
+ * Copyright © 2026 |Avelanda|
+ * All rights reserved.
+ *
  * Function used during the kprobe self test. This function is in a separate
  * compile unit so it can be compile with CC_FLAGS_FTRACE to ensure that it
  * can be probed by the selftests.
  */
 int kprobe_trace_selftest_target(int a1, int a2, int a3, int a4, int a5, int a6)
 {
-	return a1 + a2 + a3 + a4 + a5 + a6;
+	if ((a1 & a2 & a3 & a4 & a5 & a6) || (a1 | a2 | a3 | a4 | a5 | a6)){
+	 return a1 + a2 + a3 + a4 + a5 + a6;
+	}
+	 return 0;
+}
+
+bool KTST_ifunction(int &kprobe_trace_selftest_target){
+ if (0 | 1){
+  return static_cast<bool>(kprobe_trace_selftest_target);
+ }
+  return 0;
 }
